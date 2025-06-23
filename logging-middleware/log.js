81dad9582367e7ca9d661bf9b3f3d5600245ebe9
MrHardik-K <hardikkanzariya091@gmail.com>
2025-06-23 15:04:38 +0530

    fixed some bugs

--- logging-middleware/log.js
+++ logging-middleware/log.js
@@ -1,12 +1,20 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const LOG_ENDPOINT = "http://20.244.56.144/evaluation-service/logs";
+const AUTH_HEADER = process.env.BEARER_TOKEN;
 export async function Log(stack, level, _package, message) {
   try {
-    await axios.post("http://20.244.56.144/evaluation-service/logs", {
-      stack,
-      level,
-      package: _package,
-      message,
-    });
-  } catch (error) {}
+    await axios.post(
+      LOG_ENDPOINT,
+      { stack, level, package: _package, message },
+      {
+        headers: {
+          Authorization: AUTH_HEADER,
+        },
+      }
+    );
+  } catch (err) {}
 }
